bbd7d24e0f7ace08fbd9d382bed1ba1b4717dbfa
Brad <Brad@DESKTOP-HMKPLVE.r1805.tciii.org.tw>
2023-11-17 12:33:02 +0800

    11171232

---
+++ Java73/src/tw/brad/tutor/Brad01.java
@@ -0,0 +1,24 @@
+package tw.brad.tutor;
+
+public class Brad01 {
+
+	public static void main(String[] args) {
+		// byte, short, int, long
+		// byte 2^8 => 256 => -128 ~ 127
+		byte var1;	// [a-zA-Z$_][a-zA-Z0-9$_]*
+		byte $$$, $_$ = 123, 變數 = -12;
+		System.out.println($_$);
+		System.out.println(變數);
+		//System.out.println(var1);
+		// short 2^16 => 65536 => -32768 ~ 32767
+		// int 2^32 => 42... => 4G
+		// long 2^64
+		byte var2 = 1, var3 = 2;
+		int var4 = var2 + var3;	// byte, short, int => int
+		byte a = 127;
+		a += 3;
+		System.out.println(a);
+		
+	}
+
+}
---
+++ Java73/src/tw/brad/tutor/Brad02.java
@@ -0,0 +1,13 @@
+package tw.brad.tutor;
+
+import java.util.Scanner;
+
+public class Brad02 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		System.out.println(a);
+	}
+
+}
