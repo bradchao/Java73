bc37701aeccb9cf1336e20fff7922d50ab7b5de0
Brad <Brad@DESKTOP-HMKPLVE.r1805.tciii.org.tw>
2023-11-17 16:29:29 +0800

    11

--- Java73/src/tw/brad/tutor/Brad07.java
+++ Java73/src/tw/brad/tutor/Brad07.java
@@ -5,7 +5,7 @@ public class Brad07 {
 	public static void main(String[] args) {
 		// loop
 		int a = 0;
-		for ( printBrad(); a < 10 ; printLine()) {
+		for ( a = 1; a < 10 ; printLine()) {
 			System.out.println(a++);
 		}
 	}
