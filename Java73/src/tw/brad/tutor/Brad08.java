9df3f1549702007fbd7094cbd7a9c977836e1186
Brad <Brad@DESKTOP-HMKPLVE.r1805.tciii.org.tw>
2023-11-20 10:22:34 +0800

    11201022

---
+++ Java73/src/tw/brad/tutor/Brad08.java
@@ -0,0 +1,15 @@
+package tw.brad.tutor;
+
+public class Brad08 {
+
+	public static void main(String[] args) {
+
+		int[] a;
+		int[] b = new int[3];
+		System.out.println(b[0]);
+		System.out.println(b[1]);
+		System.out.println(b[2]);
+
+	}
+
+}
