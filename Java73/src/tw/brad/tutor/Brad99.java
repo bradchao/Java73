db05efdcc915518155b992104aad0e87166783cb
Brad <Brad@DESKTOP-HMKPLVE.r1805.tciii.org.tw>
2023-11-20 09:36:28 +0800

    11200936

---
+++ Java73/src/tw/brad/tutor/Brad99.java
@@ -0,0 +1,23 @@
+package tw.brad.tutor;
+
+public class Brad99 {
+
+	public static void main(String[] args) {
+		final int START = 1;
+		final int ROWS = 1;
+		final int COLS = 9;
+		
+		for (int k=0; k<ROWS; k++) {
+			for (int j=1; j<= 9; j++) {
+				for (int i=START; i< START+COLS; i++) {
+					int newi = i + k * COLS;
+					int r = newi * j;
+					System.out.printf("%d x %d = %d\t", newi, j, r);
+				}
+				System.out.println();
+			}
+			System.out.println("----");
+		}
+	}
+
+}
