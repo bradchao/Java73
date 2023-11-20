d586645a31c682b1c63b0f9019616779cd7500c4
Brad <Brad@DESKTOP-HMKPLVE.r1805.tciii.org.tw>
2023-11-20 15:40:10 +0800

    11201540

--- Java73/src/tw/brad/tutor/PokerV3.java
+++ Java73/src/tw/brad/tutor/PokerV3.java
@@ -1,9 +1,11 @@
 package tw.brad.tutor;
 
+import java.util.Arrays;
+
 public class PokerV3 {
 
 	public static void main(String[] args) {
-		int num = 10;
+		int num = 52;
 		int[] poker = new int[num];
 		for (int i=0; i< poker.length; i++) poker[i] = i;
 		
@@ -15,9 +17,29 @@ public class PokerV3 {
 			poker[i] = temp;
 		}
 		
-		for (int v : poker) {
-			System.out.println(v);
+//		for (int v : poker) {
+//			System.out.println(v);
+//		}
+//		
+//		System.out.println("---");
+		
+		int[][] players = new int[4][13];
+		for (int i=0; i<num; i++) {
+			players[i%4][i/4] = poker[i];
 		}
+		
+		String[] lunch = {"黑桃","紅心","方塊","梅花"};
+		String[] value = {"A ","2 ","3 ","4 ","5 ","6 ","7 ","8 "
+				,"9 ","10","J ","Q ","K "};
+		for (int[] player : players) {
+			Arrays.sort(player);
+			for (int card : player) {
+				System.out.printf("%s%s  ", lunch[card/13], value[card%13]);
+			}
+			System.out.println();
+		}
+		
+		
 		
 		
 	}
