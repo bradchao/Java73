a621894fe6e0bc4796d46a887b8a1c680b0c64f4
Brad <Brad@DESKTOP-HMKPLVE.r1805.tciii.org.tw>
2023-11-22 11:18:02 +0800

    11221117

--- Java73/src/tw/brad/tutor/GuessNumber.java
+++ Java73/src/tw/brad/tutor/GuessNumber.java
@@ -1,6 +1,8 @@
 package tw.brad.tutor;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,10 +10,11 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class GuessNumber extends JFrame{
+public class GuessNumber extends JFrame implements ActionListener{
 	private JButton guess;
 	private JTextField input;
 	private JTextArea log;
+	private String answer;
 	
 	public GuessNumber() {
 		// super();
@@ -24,6 +27,8 @@ public class GuessNumber extends JFrame{
 		input = new JTextField();
 		log = new JTextArea();
 		
+		guess.addActionListener(this);
+		
 		top.add(guess, BorderLayout.EAST);
 		top.add(input, BorderLayout.CENTER);
 		
@@ -33,11 +38,39 @@ public class GuessNumber extends JFrame{
 		setSize(800, 600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		answer = createAnswer(3);
+		System.out.println(answer);
+	}
+	
+	private String createAnswer(int d) {
+		int num = 10;
+		int[] poker = new int[num];
+		for (int i=0; i< poker.length; i++) poker[i] = i;
+		
+		for (int i = num -1;  i > 0; i--) {	// num .... 1
+			int rand = (int)(Math.random()*(i+1));
+			// poker[rand] <-> poker[i];
+			int temp = poker[rand];
+			poker[rand] = poker[i];
+			poker[i] = temp;
+		}	
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i<d; i++)sb.append(poker[i]);
+		
+		return sb.toString();
 	}
 	
 	public static void main(String[] args) {
 		new GuessNumber();
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//System.out.println(createAnswer(4));
+		
 	}
 
 }
