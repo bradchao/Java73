1a1677675f94ddb0ab05747f00a98c0a12db315e
Brad <Brad@DESKTOP-HMKPLVE.r1805.tciii.org.tw>
2023-11-29 14:13:26 +0800

    11291213

--- Java73/src/tw/brad/classes/GamePanel.java
+++ Java73/src/tw/brad/classes/GamePanel.java
@@ -3,19 +3,24 @@ package tw.brad.classes;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javax.imageio.ImageIO;
+import javax.naming.LinkLoopException;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
 	private BallImg[] ballImg;
 	private int viewW, viewH;
 	private Timer timer;
+	private LinkedList<BallTask> balls;
 	
 	public GamePanel() {
 		setBackground(Color.YELLOW);
@@ -33,11 +38,32 @@ public class GamePanel extends JPanel{
 			System.exit(0);
 		}
 		
+		balls = new LinkedList<>();
 		
+		addMouseListener(new MyListener());
 		
 		timer = new Timer();
 		//timer.schedule(new BallTask(), 1*1000, 10);
 		timer.schedule(new RefreshTask(), 0, 16);
+	}
+	
+	private class MyListener extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			createBall(e.getX(), e.getY());
+		}
+	}
+	
+	private void createBall(int x, int y) {
+		int ballIndex = (int)(Math.random()*3);
+		
+		BallTask ball = new BallTask(
+				x - (int)(ballImg[ballIndex].w / 2.0), 
+				y - (int)(ballImg[ballIndex].w / 2.0), 
+				(int)(Math.random()*6), (int)(Math.random()*6), 
+				ballIndex);
+		timer.schedule(ball, 500, 30);
+		balls.add(ball);
 	}
 	
 	private class RefreshTask extends TimerTask {
@@ -79,8 +105,9 @@ public class GamePanel extends JPanel{
 		
 		Graphics2D g2d = (Graphics2D)g;
 		
-	
-		//g2d.drawImage(ball0, ballX, ballY, null);
+		for (BallTask ball: balls) {
+			g2d.drawImage(ballImg[ball.ballIndex].img, ball.x, ball.y, null);
+		}
 	}
 }
 class BallImg {
